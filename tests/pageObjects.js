d76f51538189bc33e3645f3e28aaeba8a58d0a68
gregdorward <greg.dorward@red-badger.com>
2018-07-09 11:00:44 +0100

    initial commit

---
+++ tests/pageObjects.js
@@ -0,0 +1,12 @@
+
+module.exports = new Map()
+    .set('productsFound', ".products-found")
+    .set('sortFeature', ".sort")
+    .set('sizes', ".filters")
+    .set('XS', "#root > div > main > div.filters > div:nth-child(2) > label > span")
+    .set('trolleyIcon', ".bag")
+    .set('emptyState', ".shelf-empty")
+    .set('addToTrolley', "#root > div > main > div.shelf-container > div:nth-child(2) > div.shelf-item__buy-btn")
+    .set('trolleyContents', ".float-cart__shelf-container")
+    .set('closeTrolley', ".float-cart__close-btn");
+
